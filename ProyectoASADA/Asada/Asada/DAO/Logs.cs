4cda98da2e56df2e87b27927fe88e094607b36b1
Luis07 <luisbrenes250594@gmail.com>
2016-01-07 21:13:25 -0800

    clase manejo de errores
    nos va a permitir llevar un registro de los errores que puedan ocurrir

---
+++ ProyectoASADA/Asada/Asada/DAO/Logs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asada.DAO
+{
+    public class Logs
+    {
+        public Logs()
+        {
+
+        }
+
+        public void Log(string error)
+        {
+            try
+            {
+                string path = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location).Replace("\\bin\\Debug", "\\Notifications\\Exceptions.txt");
+                StreamWriter sw = new StreamWriter(path, true);
+                sw.WriteLine(error);
+                sw.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+}
